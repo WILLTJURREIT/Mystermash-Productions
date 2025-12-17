e96b9cc090100fb805c82f0e1d9c7755a3dec65c
WILLTJURREIT <wjurreit@northislandcollege.ca>
2025-12-17 09:26:57 -0800

    Create UserController.php
    Next, Add view

---
+++ app/controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+// This user controller handles pages that need a logged in user, right now, this is only the dashboard later on, it will handle posts, profile, and more...
+
+class UserController
+{
+    private PDO $pdo;
+
+    //this PDO connection comes from index.php
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    // user dashboard, this needs to be only accessible to logged in users.
+    public function dashboard()
+    {
+        requireLogin();
+    }
+}
